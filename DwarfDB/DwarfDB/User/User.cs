807e170ffd70ba2c4aa106132e4c06bbf310d19d
devgopher <igevdo@mail.ru>
2015-10-29 17:35:30 +0300

    + user.cs

--- DwarfDB/DwarfDB/User/User.cs
+++ DwarfDB/DwarfDB/User/User.cs
@@ -4,6 +4,7 @@
  * Время: 15:17
  */
 using System;
+using System.IO;
 
 namespace DwarfDB.User
 {
@@ -12,8 +13,12 @@ namespace DwarfDB.User
 	/// </summary>
 	public class User
 	{
-		public User()
+		private User()
 		{
+		}
+		
+		static User() {
+			users_file_path = Config.Config.Instance.DataDirectory+"/.users";
 		}
 		
 		/// <summary>
@@ -24,6 +29,42 @@ namespace DwarfDB.User
 			return new User();
 		}
 		
+		/// <summary>
+		/// Getting user from a 'users' list
+		/// </summary>
+		/// <param name="login"></param>
+		/// <returns></returns>
+		public static User Get( string login ) {
+			var strg = FindLogin( login );
+			if ( strg != null ) {
+				var tmp = strg.Split(':');
+				var user = new User();
+				user.Credentials.Login = login;
+				user.Credentials.hashed_pwd = tmp[1];
+				return user;
+			}
+			return null;
+		}
+		
+		/// <summary>
+		/// Creaing a new user
+		/// </summary>
+		/// <param name="login"></param>
+		/// <param name="passwd"></param>
+		/// <returns></returns>
+		public static User New( string login, string passwd ) {
+			if ( FindLogin( login ) != null )
+				throw new UserException("User with a such login already exists!");
+			var user = new User();
+			user.Credentials.Login = login;
+			user.Credentials.Password = passwd;
+			AddToUsersList( user );
+			return user;
+		}
+		
+		/// <summary>
+		/// User access credentials
+		/// </summary>
 		public UserCredentials Credentials {
 			get {
 				return user_credentials;
@@ -33,6 +74,63 @@ namespace DwarfDB.User
 			}
 		}
 		
+		/// <summary>
+		/// Creating a file with a list of users
+		/// </summary>
+		private static void CreateUsersList() {
+			if ( !File.Exists( users_file_path ) ) {
+				File.Create( users_file_path );
+			}
+		}
+		
+		/// <summary>
+		/// Adding a new user to userlist file
+		/// </summary>
+		/// <param name="_user"></param>
+		private static void AddToUsersList( User _user ) {
+			CreateUsersList();
+			if ( FindLogin(_user.Credentials.Login) != null ) {
+				using ( var fs = new FileStream( users_file_path, FileMode.Append, FileAccess.Write ) ) {
+					var sr = new StreamReader( fs );
+					var strg = "\r\n" + _user.Credentials.Login + ":" + _user.Credentials.hashed_pwd;
+				}
+			}
+		}
+		
+		/*	private static void RemoveFromUsersList( User _user ) {
+			int user_strg_num = FindLogin(_user.Credentials.Login);
+			if ( user_strg_num  > -1 ) {
+				using ( var fs = new FileStream( users_file_path, FileMode.Append, FileAccess.Write ) ) {
+					var sw = new StreamWriter( fs );
+					sw.
+				}
+			}
+		}
+		 */
+		
+		/// <summary>
+		/// Seeking for a given login in userlist file
+		/// </summary>
+		/// <param name="login"></param>
+		/// <returns></returns>
+		private static string FindLogin( string login ) {
+			using ( var fs = new FileStream( users_file_path, FileMode.Open, FileAccess.Read ) ) {
+				var sr = new StreamReader( fs );
+				string strg = String.Empty;
+				int strg_number = 0;
+				while ( (strg = sr.ReadLine()) != null ) {
+					var tmp = strg.Split(':');
+					++strg_number;
+					if ( tmp.Length > 1 )
+						if ( tmp[0].Trim() == login.Trim() )
+							return strg;
+				}
+			}
+			return null;
+		}
+		
+		
+		private static string users_file_path = "";
 		private UserCredentials user_credentials = null;
 		private UserPermissions user_permissions = null;
 	}
---
+++ DwarfDB/DwarfDB/User/UserException.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Пользователь: igor.evdokimov
+ * Дата: 29.10.2015
+ * Время: 17:11
+ */
+using System;
+using System.Runtime.Serialization;
+
+namespace DwarfDB.User
+{
+	/// <summary>
+	/// Description of UserException.
+	/// </summary>
+	public class UserException : Exception, ISerializable
+	{
+		public UserException()
+		{
+		}
+
+	 	public UserException(string message) : base(message)
+		{
+		}
+
+		public UserException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		// This constructor is needed for serialization.
+		protected UserException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
