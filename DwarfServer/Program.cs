8e0f82d13c75b13872cb9950fdcd3649b7594bbd
devgopher <igevdo@mail.ru>
2015-11-11 12:31:00 +0300

    Dwarf Server project added

---
+++ DwarfServer/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Пользователь: igor.evdokimov
+ * Дата: 11.11.2015
+ * Время: 12:15
+ */
+using System;
+
+namespace DwarfServer
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("DwarfDB server starting...");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
