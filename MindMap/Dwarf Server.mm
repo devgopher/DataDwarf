c97f58d42855ad4958daa1dbaab49ee717ce1844
devgopher <igevdo@mail.ru>
2015-11-12 23:17:15 +0300

    Some mindmaps added

---
+++ MindMap/Dwarf Server.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1447359101952" ID="ID_1106499995" MODIFIED="1447359179133" TEXT="Dwarf Server">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1447359219620" ID="ID_1691911428" MODIFIED="1447359305625" POSITION="right" TEXT="Listener"/>
+<node CREATED="1447359242748" ID="ID_654525902" MODIFIED="1447359256602" POSITION="left" TEXT="Query Format"/>
+<node CREATED="1447359369916" ID="ID_228575481" MODIFIED="1447359375973" POSITION="right" TEXT="Response format"/>
+</node>
+</map>
